40fecbee243e25abf33c3aff967967430603a867
carvalhinhojulia <00001089516393sp@al.educacao.sp.gov.br>
2024-11-19 12:11:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Química',
+    'exemplos de termoquímica?',
+    ': A solidificação da água em gelo é uma mudança de estado físico que ocorre quando há liberação de calor. Os motores a explosão dos automóveis funcionam a partir da energia liberada pela reação de combustão de etanol ou de derivados do petróleo, como a gasolina e o óleo diesel.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos fusos horário existem na Rússia?',
+    'Na Rússia existem 11 fusos horários'
+)
+
+criaCartao(
+    'História',
+    'Quem foi Zumbi dos Palmares?',
+    'Foi um dos principais representantes da resistência negra à escravidão na época do Brasil Colonial. Foi líder do Quilombo dos Palmares, comunidade livre formada por escravos fugitivos das'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
